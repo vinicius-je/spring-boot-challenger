f40866eaec31e226875cfa73e730659badd86fa8
vinicius-je <vinicius.je99@gmail.com>
2023-07-13 15:30:36 -0300

    feat: add todo controller

---
+++ src/main/java/br/com/viniciusestevam/spring/boot/challenger/controller/TodoController.java
@@ -0,0 +1,37 @@
+package br.com.viniciusestevam.spring.boot.challenger.controller;
+
+import br.com.viniciusestevam.spring.boot.challenger.entity.Todo;
+import br.com.viniciusestevam.spring.boot.challenger.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoRepository){
+        this.todoService = todoRepository;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
