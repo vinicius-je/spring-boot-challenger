43e118e970980b7bb81ef41f199a42e9585eb053
vinicius-je <vinicius.je99@gmail.com>
2023-07-13 15:30:24 -0300

    fix: add id as a param in delete function

--- src/main/java/br/com/viniciusestevam/spring/boot/challenger/service/TodoService.java
+++ src/main/java/br/com/viniciusestevam/spring/boot/challenger/service/TodoService.java
@@ -11,28 +11,28 @@ import java.util.List;
 public class TodoService {
      
     private TodoRepository todoRepository;
-
+    
     public TodoService(TodoRepository todoRepository){
         this.todoRepository = todoRepository;
     }
-
+    
     public List<Todo> create(Todo todo){
         todoRepository.save(todo);
         return list();
     }
-
+    
     public List<Todo> list(){
         Sort sort = Sort.by("prioridade").descending().and(
                 Sort.by("nome").ascending()
         );
         return todoRepository.findAll(sort);
     }
-
+    
     public List<Todo> update(Todo todo){
         todoRepository.save(todo);
         return list();
     }
-
+    
     public List<Todo> delete(Long id){
         todoRepository.deleteById(id);
         return list();
