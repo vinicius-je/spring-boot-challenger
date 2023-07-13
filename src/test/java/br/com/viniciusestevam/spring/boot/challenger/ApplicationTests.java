417c3265354fea7e9a7c8d6c1b33c95d8e135d5b
vinicius-je <vinicius.je99@gmail.com>
2023-07-13 14:43:39 -0300

    initial commit

---
+++ src/test/java/br/com/viniciusestevam/spring/boot/challenger/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusestevam.spring.boot.challenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
