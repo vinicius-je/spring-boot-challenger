1184580eb94a3b55dccbf696ac0662b1166944ee
vinicius-je <vinicius.je99@gmail.com>
2023-07-13 16:32:59 -0300

    feat: add todo tests

--- src/main/java/br/com/viniciusestevam/spring/boot/challenger/entity/Todo.java
+++ src/main/java/br/com/viniciusestevam/spring/boot/challenger/entity/Todo.java
@@ -13,6 +13,13 @@ public class Todo {
     private boolean realizado;
     private int prioridade;
 
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
     public Long getId() {
         return id;
     }
--- src/test/java/br/com/viniciusestevam/spring/boot/challenger/ApplicationTests.java
+++ src/test/java/br/com/viniciusestevam/spring/boot/challenger/ApplicationTests.java
@@ -1,13 +1,43 @@
 package br.com.viniciusestevam.spring.boot.challenger;
 
+import br.com.viniciusestevam.spring.boot.challenger.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testeCreateTodoSuccess(){
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
+
+		webTestClient
+				.post()
+				.uri("todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+				.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+				.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+	@Test
+	void testeCreateTodoFailure (){
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(
+						new Todo("", "", false, 0))
+				.exchange()
+				.expectStatus().isBadRequest();
 	}
 
 }
